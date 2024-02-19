9571f9f2c173ce8fd754e3a52d580cd84fc91fd3
LSierraOsorio <luis.asierraosorio99@gmail.com>
2024-02-18 19:29:11 -0800

    added children routing

--- recipe-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -5,14 +5,9 @@ import { RecipeService } from '../../recipe.service';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrl: './recipe-item.component.css'
+  styleUrl: './recipe-item.component.css',
 })
 export class RecipeItemComponent {
   @Input() recipe: Recipe;
-  
-  constructor(private recipeService: RecipeService){}
-
-  onSelected(){
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
+  @Input() index: number;
 }
